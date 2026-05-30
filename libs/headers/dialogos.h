a85ff9b6b891964bbabd37492d7dfd4af6036bb9
Leo1551 <leonardomacedobarboza8@gmail.com>
2026-05-30 02:25:41 -0300

    correção: foi criado a função no.h para futura implementação

--- libs/headers/dialogos.h
+++ libs/headers/dialogos.h
@@ -2,9 +2,11 @@
 #define DIALOGOS_H
 
 #include "pokemon.h"
+#include "player.h"
 
 int* get_tamanho_times(void);
 Pokemon* criar_time(int tamanho);
 int* captar_stats(int max_por_stat, int max_total);
+void iniciar_batalha(Player player1, Player player2);
 
 #endif 
